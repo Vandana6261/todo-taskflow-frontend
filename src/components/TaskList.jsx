c52dd16c02a3db4345e79dd498859fe9e9f7aadf
Vandana6261 <vvandanapatidar992@gmail.com>
2026-01-23 11:42:17 +0530

    Write functionn to display all Tasks.

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -3,18 +3,35 @@
 
 import React, { useState } from 'react'
 import AddTaskDialogBox from './AddTaskDialogBox'
+import TaskItem from './TaskItem';
+import { useTodo } from '../hooks/useTodo';
 
 function TaskList() {
     const [showDialogBox, setShowDialogBox] = useState(false);
+    const {tasks, addTask} = useTodo()
+    console.log(tasks)
+
   return (
     <>
+
+      {!tasks ? 
+        <div>
+          <p>No todos to display</p>
+        </div>
+        :
+        <div>
+          {tasks.map((task) => {
+            return <TaskItem task={task} />
+          })}
+        </div>
+      }
       <div className='max-w-screen min-h-screen bg-transparent'>
         <button 
         onClick={() => setShowDialogBox(true)}
         className='btn hoverBase'
         >Add Task</button>
 
-          <AddTaskDialogBox showDialogBox={showDialogBox} setShowDialogBox={setShowDialogBox}/>
+        <AddTaskDialogBox addTask={addTask} showDialogBox={showDialogBox} setShowDialogBox={setShowDialogBox}/>
       </div>
     </>
   )
