fe3784d9740af045e8f1c3b5a40b56b3ea3f41ae
Vandana6261 <vvandanapatidar992@gmail.com>
2026-01-26 18:59:00 +0530

    New component add.

---
+++ src/components/TodoApp.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from 'react'
+import TaskList from './TaskList';
+import { GiHamburgerMenu } from "react-icons/gi";
+import { FiCheckSquare } from "react-icons/fi";
+
+
+function TodoApp() {
+  const [showCategory, setShowCategory] = useState(false)
+  return (
+    <>
+      <div className='max-w-screen h-screen flex flex-col'>
+        {/* header */}
+        <header className='flex justify-start items-center h-auto p-4 border-b border-gray-300 gap-8'>
+          <span className='text-2xl'>
+            <GiHamburgerMenu />
+          </span>
+          <div className='flex items-center gap-4 text-2xl '>
+            <span>
+              <FiCheckSquare />
+            </span>
+            <h2>TaskFlow</h2>
+          </div>
+        </header>
+
+
+        <div className='flex flex-1 items-center overflow-hidden'>
+          {/* category */}
+        <div className='flex-1 border-r border-gray-300 px-2 h-full '>
+          category
+        </div>
+
+        {/* main */}
+        <div className='flex-3 border-r border-gray-300 px-2 h-full '>
+          <TaskList />
+        </div>
+
+        {/* todoUpdate */}
+        <div className='flex-1 border-r border-gray-300 px-2 h-full '>
+          Todo Update
+        </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default TodoApp
+
+  {/* <TaskList /> */}
