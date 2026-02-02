c3c798c9404b4d932c17b45f12ffe0c77d10efb4
Vandana6261 <vvandanapatidar992@gmail.com>
2026-02-02 12:39:17 +0530

    Add update functionality and also add cancel button.

--- src/components/TaskDetails.jsx
+++ src/components/TaskDetails.jsx
@@ -4,57 +4,91 @@ import { MdDeleteForever } from "react-icons/md";
 import { RxUpdate } from "react-icons/rx";
 import useTodoContext from '../context/TodoContext';
 
-function TaskDetails() {
+function TaskDetails({setIsUpdate, isUpdate}) {
   const {selectId, setSelectId, tasks, deleteTask, updateTask} = useTodoContext();
+  const [updatedData, setUpdatedData] = useState({})
 
+  useEffect(() => {
+    const task = tasks.find((eachTask) => eachTask.id === selectId);
+    setUpdatedData(task)
+  }, [selectId])
   
+
+  const handleChange = (e) => {
+    const {name, value} = e.target;
+    setUpdatedData(prev => ({...prev, [name]: value}))
+  }
+
+  const handleUpdate = () => {
+    console.log("Update is clicked")
+    updateTask(updatedData);
+    console.log("q24")
+    setIsUpdate(!isUpdate)
+  }
+
+  const handleDelete = (id) => {
+    const isConfirmed = window.confirm("Are you sure you want to delete?");
+    if(isConfirmed) {
+      deleteTask(id);
+      // alert("Todo deleted successfully");
+    }
+    return;
+  }
+
   return (
     <>
-      <div>
-        {
-          selectId === null ? 
-          <div>
-            <span>
-              <IoMdTime /> 
-            </span>
-            <h3>No Task Selected</h3>
-            <p>Click on a task to view and edit its details</p>
-          </div>
-          : 
-          // <div>Hello</div>
-          <div className='flex flex-col gap-4 bg-amber-100 min-h-full'>
+      <div className='min-h-screen min-w-screen fixed backdrop-blur-sm flex justify-center items-center top-0 left-0 z-9999'>
+        {/* <div>Hello</div> */}
+          <div className='flex flex-col gap-4 bg-blue-900/25 min-h-full rounded-xl p-4 '>
             {/* 1st part */}
             <div className='flex-1 border-b border-gray-300 rounded px-4 py-2 flex justify-between'>
               <p>Task Details</p>
-              <span className='border h-8 w-8 flex justify-center items-center rounded-[100%] bg-gray-300 '>X</span>
+              <span className='border h-8 w-8 flex justify-center items-center rounded-[100%] bg-gray-300 cursor-pointer'
+                onClick={() => setIsUpdate(!isUpdate)}
+              >X</span>
             </div>
 
             {/* 2nd part */}
             <div className='flex-3 max-h-[70vh] overflow-y-auto border-b border-gray-300 '>
               <div className='flex flex-col gap-2 '>
                 <div className='mx-2'>
-                  <h4 className='heading4 '>Title: </h4>
-                  <input type="text" 
-                  // value={}
+                  <label 
+                  className='heading4 ' 
+                  htmlFor='title'>
+                    Title: 
+                  </label>
+                  <input 
+                  type="text" id='title' name='title'
+                  value={updatedData.title}
                   className='inputBase px-2 py-1 text-sm'
                   onChange={(e) => handleChange(e)}
                   />
                 </div>
+
                 <div className='mx-2'>
-                  <h4 className='heading4 '>Description:</h4>
+                  <label 
+                  className='heading4'
+                  htmlFor="description" id='description'>
+                    Description
+                  </label>
                   <textarea name="description" id="description" 
-                  // value={}
+                  value={updatedData.description}
                   className='inputBase px-2 py-1 text-sm '
                   onChange={(e) => handleChange(e)}
                   rows={4}
                   ></textarea>
                 </div>
+
                 <div className='mx-2'>
-                  <h4 className='heading4 '>Priority:</h4>
+                  <label 
+                  className='heading4 '
+                  htmlFor='priority'
+                  >
+                    Priority:
+                  </label>
                   <p>
                     <select name="priority" id="priority" 
-                    // 
-                    // value={}
+                    value={updatedData.priority}
                     className='inputBase text-sm '
                     onChange={(e) => handleChange(e)}
                     >
@@ -65,10 +99,15 @@ function TaskDetails() {
                   </p>
                 </div>
                 <div className='mx-2'>
-                  <h4 className='heading4 '>Status:</h4>
+                  <label 
+                  className='heading4 '
+                  htmlFor='status'
+                  >
+                    Status:
+                  </label>
                   <p>
                     <select name="status" id="status" 
-                    // value={}
+                    value={updatedData.status}
                     className='inputBase text-sm '
                     onChange={(e) => handleChange(e)}
                     >
@@ -79,10 +118,15 @@ function TaskDetails() {
                   </p>
                 </div>
                 <div className='mx-2'>
-                  <h4 className='heading4'>Category:</h4>
+                  <label 
+                  className='heading4'
+                  htmlFor='category'
+                  >
+                    Category:
+                    </label>
                   <p>
                     <select name="category" id="category" 
-                    // value={}
+                    value={updatedData.category}
                     className='inputBase text-sm '
                     onChange={(e) => handleChange(e)}
                     >
@@ -93,11 +137,16 @@ function TaskDetails() {
                   </p>
                 </div>
                 <div className='mx-2'>
-                  <h4 className='heading4 '>Due Date:</h4>
+                  <label 
+                  className='heading4 '
+                  htmlFor='date'
+                  >
+                    Due Date:
+                    </label>
                   <p>
                     <input 
                       type="date" id='date' name='date'
-                      // value={}
+                      value={updatedData.dueDate}
                       onChange={(e) => handleChange(e)}
                       className='inputBase text-sm '
                   />
@@ -114,24 +163,36 @@ function TaskDetails() {
             {/* 3rd part  */}
             <div className='flex-1 flex items-center justify-between gap-4 px-2 self-end '>
               <button className='btn hoverBase flex items-center gap-2'
-              // onClick={() => handleDelete(task[0].id)}
+              onClick={() => setIsUpdate(!isUpdate)}
+              >
+                <span>
+                  <MdDeleteForever />
+                </span>
+                <span>
+                  Cancel
+                </span>
+              </button>
+              
+              <button className='btn hoverBase flex items-center gap-2'
+              onClick={() => handleDelete(updatedData.id)}
               >
                 <span>
                   <MdDeleteForever />
                 </span>
                 <span>Delete</span>
               </button>
+              
               <button className='btn hoverBase flex items-center gap-2'
-              // onClick={() => hanldeUpdate(task[0].id)}
+              onClick={() => handleUpdate()}
               >
                 <span>
                   <RxUpdate />
                 </span>
-                <span>Show Changes</span>
+                <span>Update</span>
               </button>
             </div>
           </div>
-        }
+        
       </div>
     </>
   )
