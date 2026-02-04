85e8ee385c4a7faf2046ad7258858ad32072ce3c
Vandana6261 <vvandanapatidar992@gmail.com>
2026-02-04 13:41:12 +0530

    Disable update button and add some css when todo is completed.

--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -1,14 +1,14 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useTodo } from '../hooks/useTodo'
 import useTodoContext from '../context/TodoContext'
 import TaskDetails from './TaskDetails'
 
 function TaskItem({task}) {
-  const { deleteTask, selectId, setSelectId} = useTodoContext()
+  const { deleteTask, updateTask, setSelectId} = useTodoContext()
   const [isUpdate, setIsUpdate] = useState(false);
+  const [isCompleted, setIsCompleted] = useState(task.isCompleted);
 
-    const isCompleted = task.status === "completed"
-    const [showDetails, setShowDetails] = useState(false);
+    // const isCompleted = task.status === "completed"
 
   const handleUpdate = () => {
     setIsUpdate(!isUpdate)
@@ -20,16 +20,28 @@ function TaskItem({task}) {
     if(isConfirmed) 
       deleteTask(task.id)
   }
+  
+  useEffect(() =>{
+
+  }, [isCompleted])
   return (
     <>
-      <div tabIndex={0} className='py-1 px-4 border border-gray-300 rounded mb-4 hover:border-gray-400 focus:border-blue-700 '
+      <div tabIndex={0} className={`py-1 px-4 border border-gray-300 rounded mb-4 hover:border-gray-400`}
+      onClick={() => setSelectId(task.id)}
       >
-        <div className='flex flex-col gap-4 mt-1 border border-gray-300 rounded-md p-2'>
+        <div className={`flex flex-col gap-4 mt-1 border border-gray-300 rounded-md p-2 ${isCompleted ? "line-through text-gray-400" : ""}`}>
             <div className='flex-1 flex gap-4'>
               <div className=''>
-                <input type="radio" />
+                <input 
+                className='cursor-progress'
+                type="checkbox"  id='checkbox' name='checkbox' checked={isCompleted}
+                onChange={(e) => {
+                  updateTask({...task, isCompleted:e.target.checked})
+                  setIsCompleted(e.target.checked)
+                }}
+                />
               </div>
-              <div className=''>
+              <div>
                 <h2>{task.title}</h2>
                 <p>{task.description}</p>
                 <div >
@@ -44,20 +56,21 @@ function TaskItem({task}) {
             </div>
         </div>
         <div className='flex '>
-          <div className='hoverBase btn border-gray-500 w-fit m-2'
+          <button className='hoverBase btn border-gray-600 w-fit m-2'
           onClick={() => handleDelete()}
           >
             Delete
-          </div>
-          <div className='hoverBase btn border-gray-500 w-fit m-2'
+          </button>
+          <button className={`hoverBase btn border-gray-600 w-fit m-2 ${isCompleted ? "line-through text-gray-400 border-gra-300 " : ""}`}
           onClick={() => handleUpdate()}
+          disabled={isCompleted}
           >
             Update
-          </div>
+          </button>
         </div>
       </div>  
 
-      <div>
+      <div className=''>
         {isUpdate ? <TaskDetails isUpdate={isUpdate} setIsUpdate={setIsUpdate}/> : ""}
       </div>
     </>
