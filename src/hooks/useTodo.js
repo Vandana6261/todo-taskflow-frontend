9d2119757515769ea44f97a1367ef31195b5db9b
Vandana6261 <vvandanapatidar992@gmail.com>
2026-02-21 12:13:45 +0530

    call api for todo (connect frontend and backend)

--- src/hooks/useTodo.js
+++ src/hooks/useTodo.js
@@ -1,84 +1,114 @@
-import { useState } from "react";
-
-
-  const defaultTask =  
-  [
-    {
-      id: "1",
-      title: "Review project proposal",
-      description:
-        "Go through the Q1 project proposal and provide feedback to the team.",
-      priority: "high",
-      status: "pending",
-      category: "Work",
-      dueDate: "2026-02-01",
-      isCompleted: false,
-      createdAt: new Date(Date.now() - 86400000), // yesterday date
-    },
-    {
-      id: "2",
-      title: "Buy groceries",
-      description: "Milk, eggs, bread, vegetables",
-      priority: "medium",
-      status: "Inprogress",
-      category: "Personal",
-      isCompleted: true,
-      createdAt: new Date(Date.now() - 172800000),
-    },
-    {
-      id: "3",
-      title: "Morning workout",
-      priority: "low",
-      status: "complete",
-      category: "Shopping",
-      isCompleted: true,
-      createdAt: new Date(Date.now() - 259200000),
-    },
-  ];
+import { useEffect, useState } from "react";
 
   
   const generateId = () => Math.random().toString(36).substring(2, 9);
   
   export function useTodo() {
 
-    const [tasks, setTasks] = useState(defaultTask)
-    const [taskToBeShow, setTaskToBeShow] = useState(tasks)
+    const [tasks, setTasks] = useState([])
+    const [taskToBeShow, setTaskToBeShow] = useState([])
     const [selectId, setSelectId] = useState(null);
-    const [categories, setCategories] = useState(["work", "personal", "shopping"])
+    const [categories, setCategories] = useState([])
     
-    const addTask = (task) => {
-      // console.log(task);
-      let newTask = {
-        ...task,
-        id: generateId(),
-        isCompleted: false,
-        createdAt: new Date().toLocaleDateString()
-      }
-      setTasks(prev => [...prev, newTask]);
-      setTaskToBeShow(prev => [...prev, newTask]);
-      if(!categories.includes(task.category)) {
-        setCategories(prev => [...prev, task.category])
+
+    async function loadTodo() {
+      // console.log("loadTodo called")
+      try {
+        const response = await fetch("http://localhost:5000/api/todo")
+        if(!response.ok) {
+          console.log("Internal server Error");
+          console.log("Error status : ", response.status);
+        }
+        else {
+          const data = await response.json();
+          setTasks(prev => [...data])
+          setTaskToBeShow(prev => [...data])
+          setCategories(prev => data.map(todo => todo.category))
+        }
+      } catch (error) {
+        console.log("Error occured while loading TODO");
+        console.log(error)
       }
     }
-    // console.log(tasks)
 
-    const deleteTask = (taskId) => {
-      console.log("Delete task called")
-      let newTasks = tasks.filter(item => item.id !== taskId);
-      setTasks(newTasks)
-      setTaskToBeShow(newTasks);
-      
+    useEffect(() => {
+      loadTodo();
+    }, [])
+
+    async function addTask(task) {
+      try {
+        // console.log("addTask called")
+        const response = await fetch("http://localhost:5000/api/todo", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(task)
+        })
+        
+        if(!response.ok) {
+          console.log("Error response is not ok: ", response.status)
+          return;
+        }
+        else {
+          const data = await response.json();
+          await loadTodo();
+        }
+      } catch (error) {
+        console.log("Error occured while adding task");
+        console.log(error)
+      }
     }
 
-    const updateTask = (task) => {
-      let newTasks = tasks.map(eachTask => {
-        if(eachTask.id === task.id) {
-          return {...task}
-        } 
-        else return eachTask;
-      })
-      setTasks(newTasks);
-      setTaskToBeShow(newTasks)
+    async function deleteTask(taskId, task) {
+      // console.log("deleteTask is called", taskId);
+      try {
+        const response = await fetch(`http://localhost:5000/api/todo/${taskId}`, {
+          method: "DELETE",
+          headers: {
+            "content-Type": "application/json"
+          },
+          body: JSON.stringify(task)
+        })
+        if(!response.ok) {
+          console.log("Error response is not ok: ", response.status);
+          console.log(response)
+          return;
+        }
+        else {
+          const data = await response.json();
+          await loadTodo();
+        }
+      } catch (error) {
+        console.log("Error occured while deleting todo")
+        console.log(error)
+      }
+    }
+
+    async function updateTask(taskId, task) {
+      // console.log("Update task called")
+      try {
+        const response = await fetch(`http://localhost:5000/api/todo/${taskId}`, {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(task)
+        })
+
+        if(!response.ok) {
+          console.log("Error response is not ok: ", response.status);
+          console.log(response);
+          return;
+        }
+        else {
+          const data = await response.json();
+          await loadTodo()
+        }
+      } catch (error) {
+        console.log("Error occured while updating todo: ", error.status);
+        console.log(error)
+      }
     }
 
     const searchTask = (text) => {
