82104c7569982698bd281bc373f49dc13fb0f251
Vandana6261 <vvandanapatidar992@gmail.com>
2026-03-20 12:14:59 +0530

    style:- change the color and logo of button and some other style

--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -9,21 +9,19 @@ import { GrUpdate } from "react-icons/gr";
 
 // console.log("TaskItem rendered")
 
-function TaskItem({task}) {
-  const {tasks, updateTask, setSelectId} = useTodoContext()
+function TaskItem({ task }) {
+  const { tasks, updateTask, setSelectId } = useTodoContext()
   const [isUpdate, setIsUpdate] = useState(false);
   const [isCompleted, setIsCompleted] = useState(task.isCompleted);
   const [confirmDelete, setConfirmDelete] = useState(false);
   const [updatedData, setUpdatedData] = useState({})
 
   const styleMap = {
-    complete: "bg-green-400/20 text-green-400",
-    inprogress: "bg-blue-700/30 text-blue-700",
-    pending: "bg-red-500/20 text-red-500"
+    complete: "bg-orange-300/20 text-orange-600",
+    inProgress: "bg-blue-300/30 text-blue-600",
+    pending: "bg-purple-500/20 text-purple-500"
   }
 
-    // const isCompleted = task.status === "completed"
-  // console.log(task)
   const handleUpdate = () => {
     setSelectId(task._id)
     setIsUpdate(!isUpdate)
@@ -34,75 +32,77 @@ function TaskItem({task}) {
   const handleDelete = () => {
     setConfirmDelete(!confirmDelete)
   }
-  
-  useEffect(() =>{
-    // updateTask(selectId, {...task, isCompleted: true})
-    // console.log(task)
+
+  useEffect(() => {
+
   }, [isCompleted])
 
   return (
-    <>  
-      <div>
-        <div tabIndex={0} className={`px-4 border-b bg-[#ffffff] min-h-30 pb-2 text-sm m-1 border border-gray-300 rounded-xl shadow-[0px_5px_10px_2px_rgba(0,0,0,0.25)]  hover:shadow-[0px_5px_10px_rgba(0,0,0,0.25)] hover:border-gray-400 `}
+    <>
+      <div tabIndex={0} className={`px-4 border-b bg-[#ffffff] min-h-30 pb-2 text-sm m-1 border border-gray-300 rounded-xl shadow-[0px_5px_10px_2px_rgba(0,0,0,0.25)]  hover:shadow-[0px_5px_10px_rgba(0,0,0,0.25)] hover:border-gray-400 `}
         onClick={() => setSelectId(task._id)}
-        >
-          {/* task title, description, etc. */}
-          <div className={`flex flex-col rounded-md py-2 ${isCompleted ? "line-through text-gray-400" : ""}`}>
-              <div className='flex gap-2'>
+      >
+        {/* task title, description, etc. */}
+        <div className={`flex flex-col rounded-md py-2 ${isCompleted ? "line-through text-gray-400" : ""}`}>
+          <div className='flex gap-2'>
+            <div className=''>
+              <input
+                className='cursor-pointer'
+                type="checkbox" id='checkbox' name='checkbox' checked={isCompleted}
+                onChange={(e) => {
+                  console.log(e.target.checked)
+                  const status = task.status !== "complete" ? "complete" : "inProgress";
+                  updateTask(task._id, { ...task, isCompleted: e.target.checked, status })
+                  setIsCompleted(e.target.checked)
+                }}
+              />
+            </div>
+
+            <div className='flex flex-col w-full'>
+              <div className='flex justify-between'>
+                <h2>{task.title}</h2>
                 <div className=''>
-                  <input
-                  className='cursor-pointer'
-                  type="checkbox"  id='checkbox' name='checkbox' checked={isCompleted}
-                  onChange={(e) => {
-                    console.log(e.target.checked)
-                    updateTask(task._id, {...task, isCompleted:e.target.checked})
-                    setIsCompleted(e.target.checked)
-                  }}
-                  />
-                </div>
-                <div className='flex flex-col'>
-                  <h2>{task.title}</h2>
-                  <p className='text-gray-500'>{task.description}</p>
-                  <div className='mt-2 flex gap-3 items-center'>
-                    {task.category && <p className='bg-gray-300/30 text-gray-700 w-fit rounded px-4 '>{task.category.name}</p>}
-                    {task.status && <p className={`w-fit rounded px-4 font-semibold ${styleMap[task.status]}`}>{task.status}</p>}
-                  </div>
-                    {/* {task.dueDate && <p className='bg-purple-200 px-2 rounded '>dueDate: <br /> {task.dueDate}</p>} */}
+                  <button className='hoverBase btn mx-2 font-semibold bg-red-500/20  text-red-600 hover:text-red-600'
+                    onClick={() => handleDelete()}
+                  >
+                    <MdDeleteForever />
+                  </button>
+                  <button className={`hoverBase btn mx-2 font-semibold bg-green-600/20 text-[#027e23] ${isCompleted ? "line-through  " : ""}`}
+                    onClick={() => handleUpdate()}
+                    disabled={isCompleted}
+                  >
+                    <GrUpdate />
+                  </button>
                 </div>
               </div>
-          </div>
-
-          {/* button delete and update */}
-          <div className='flex justify-between'>
-            <div>{task.dueDate && <small className=' px-2 rounded  text-gray-500'>Due: {task.dueDate}</small>}</div>
-            <div>
-              <button className='hoverBase btn mx-2 font-semibold bg-red-300/20  text-red-600 hover:text-red-600'
-              onClick={() => handleDelete()}
-              >
-                <MdDeleteForever />
-              </button>
-              <button className={`hoverBase btn mx-2 font-semibold bg-blue-600/10 text-[#4F39F6] ${isCompleted ? "line-through  " : ""}`}
-              onClick={() => handleUpdate()}
-              disabled={isCompleted}
-              >
-                <GrUpdate />
-              </button>
+              <p className='text-gray-500'>{task.description}</p>
+              <div className='mt-2 flex gap-3 items-center'>
+                {task.category && <p className='bg-gray-300/30 text-gray-900 w-fit rounded px-4'>{task.category.name}</p>}
+                {task.status && <p className={`w-fit rounded px-4 font-semibold ${styleMap[task.status]}`}>{task.status}</p>}
+              </div>
             </div>
+
+
           </div>
         </div>
-        
-        {/* TaskDetails (update) */}
-        <div className=''>
-          {createPortal (
-            isUpdate ? <TaskDetails isUpdate={isUpdate} setIsUpdate={setIsUpdate} updatedData={updatedData} setUpdatedData={setUpdatedData} /> : "", document.body
-          )}
+
+        {/* button delete and update */}
+        <div className='flex justify-between'>
+          <div>{task.dueDate && <small className=' px-2 rounded  text-gray-500'>Due: {task.dueDate}</small>}</div>
         </div>
-        {/* confirmation (delete) */}
-        <div>
-          {createPortal (
-            confirmDelete ? <Confirmation task={task} taskId={task._id} setConfirmDelete={setConfirmDelete}/> : "", document.body
-          )}
-        </div>
+      </div>
+
+      {/* TaskDetails (update) */}
+      <div className=''>
+        {createPortal(
+          isUpdate ? <TaskDetails isUpdate={isUpdate} setIsUpdate={setIsUpdate} updatedData={updatedData} setUpdatedData={setUpdatedData} /> : "", document.body
+        )}
+      </div>
+      {/* confirmation (delete) */}
+      <div>
+        {createPortal(
+          confirmDelete ? <Confirmation task={task} taskId={task._id} setConfirmDelete={setConfirmDelete} /> : "", document.body
+        )}
       </div>
     </>
   )
