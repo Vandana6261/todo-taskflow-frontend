cf1f70bc60357f1c935ccb4020facbd93a2cfb85
Vandana6261 <vvandanapatidar992@gmail.com>
2026-04-19 12:48:31 +0530

    feat:- create routes and corresponding pages

--- src/App.jsx
+++ src/App.jsx
@@ -5,23 +5,63 @@ import './App.css'
 import TaskList from './components/TaskList'
 import TodoApp from './components/TodoApp'
 import { TodoProvider } from './context/TodoContext'
-import SignUp from './components/SignUp'
+// import SignUp from './components/SignUp'
 import Dashboard from './components/Dashboard'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+import Landing from './pages/Landing';
+import Login from './pages/Login';
+import SignUp from './pages/SignUp'
+import Home from './pages/Home'
 
 
 function App() {
 
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Landing />,
+      children: [
+        {
+          index: true,
+          element: <Home />
+        },
+        {
+          path: "/signUp",
+          element: <SignUp />
+        },
+        {
+          path: "/login",
+          element: <Login />
+        },
+        {
+          path: "/dashboard",
+          element: <Dashboard />,
+          // loader: 
+        }
+      ]
+    }
+  ])
+
   return (
     <>
-      {/* <div>hello</div> */}
-      {/* <TaskList /> */}
       <TodoProvider>
-        <TodoApp />
-        {/* <Dashboard /> */}
-        {/* <SignUp /> */}
+        <RouterProvider router = {router}/>
+        {/* <TodoApp /> */}
       </TodoProvider>
     </>
   )
 }
 
 export default App
+
+
+
+
+{/* <div>hello</div> */}
+{/* <TaskList /> */}
+{/* <TodoProvider>
+  <TodoApp />
+</TodoProvider> */}
+  {/* <Dashboard /> */}
+  {/* <SignUp /> */}
---
+++ src/pages/Landing.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { NavLink, Outlet, useNavigate } from 'react-router-dom';
+
+function Landing() {
+  const navigate = useNavigate();
+  return (
+    <>
+      <div className='max-w-full max-h-full w-auto text-center flex justify-around items-center p-2 bg-gradient-to-br from-white to-blue-50 z-10 relative'>
+        <h1 className='flex-1 text-3xl '>Todo TaskFlow</h1>
+        {/* <p className='text-gray-600'>Manage your tasks easily</p> */}
+        <div>
+          <NavLink 
+            to="/"
+            className='btn hoverBase border-none w-auto py-2 px-4  text-white font-semibold bg-[#0019f7a8] rounded-full hover:shadow-[0px_0px_20px_rgba(0,15,205,0.4)]'
+          >
+            Home
+          </NavLink>
+        </div>
+        <div className='flex-1 flex justify-center  gap-4 '>
+          <NavLink
+            to="/login"
+            className='btn hoverBase border-none w-auto py-2 px-4 text-white font-semibold bg-[#0019f7a8] rounded-full hover:shadow-[0px_0px_20px_rgba(0,15,205,0.4)]'
+            onClick={() => navigate("/login")}
+          >Login</NavLink>
+          <NavLink
+            to="/signUp"
+            className='btn hoverBase border-none w-auto py-2 px-4  text-white font-semibold bg-[#0019f7a8] rounded-full hover:shadow-[0px_0px_20px_rgba(0,15,205,0.4)]'
+            onClick={() => navigate("/signUp")}
+          >SignUp</NavLink>
+        </div>
+      </div>
+      <Outlet />
+    </>
+  )
+}
+
+export default Landing
